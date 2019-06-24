4d12954e656eee9d6ff2963be80e0c65b7a2f98c
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-24 13:28:53 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,24 @@
+def reverse_each_word(sentense)
+  new_sentense = []
+  sentense.split(" ").each do |word|
+    new_word = []
+    word.split("").reverse.each do |character|
+      new_word << character
+    end
+    new_sentense << new_word.join
+  end
+  return new_sentense.join(" ")
+end
+
+def reverse_each_word(sentense)
+
+  sentense.split(" ").collect do |word|
+    
+    word.split("").reverse.collect |character|
+    word.join
+
+  sentense.join
+end
+  
+  
+  
